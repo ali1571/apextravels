87e35cf5df4044fb2fe757caea9cbec5dc341710
Qwerty1571 <86067649+ali1571@users.noreply.github.com>
2026-02-16 15:54:43 +0500

    Update urls.py

--- travelProj/urls.py
+++ travelProj/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls), # Fixed the error here
     path('', include('travelApp.urls')), # Points to your app
-    path("__reload__/", include("django_browser_reload.urls")),
+  
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
