a6dfe2fa7be7cfa0eec6f551e52bbc4bd16935ce
Qwerty1571 <86067649+ali1571@users.noreply.github.com>
2026-02-17 14:01:25 +0500

    Update views.py

--- travelApp/views.py
+++ travelApp/views.py
@@ -5,11 +5,12 @@ from django.template.loader import render_to_string
 from django.conf import settings
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
+import resend
+from django.conf import settings
 
-@csrf_protect  # Usually not needed if middleware is enabled
+@csrf_protect
 def home(request):
     if request.method == "POST":
-        # 1. Capture the data from the frontend names
         data = {
             'pickup': request.POST.get('pickup'),
             'dropoff': request.POST.get('dropoff'),
@@ -21,38 +22,32 @@ def home(request):
             'phone': request.POST.get('phone'),
         }
 
-        # --- TERMINAL TESTING BLOCK ---
         print("\n" + "=" * 50)
         print(f"🚀 NEW LEAD RECEIVED: {data['email']}")
         print(f"📍 FROM: {data['pickup']} ➡️ TO: {data['dropoff']}")
         print(f"🚗 VEHICLE: {data['vehicle_type']} for {data['hours']} hrs")
         print(f"📞 CONTACT: {data['phone']}")
         print("=" * 50 + "\n")
-        # ------------------------------
 
-        # 2. Senior Dev Logic: Log it or Save it to DB
         html_content = render_to_string('emails/qoute_request.html', {'data': data})
 
-        email_message = EmailMessage(
-            subject=f"New Qoute Request from {data['email']}",
-            body= html_content,
-            from_email = settings.EMAIL_HOST_USER,
-            to= ["Sales@apextourtravel.com"],
-            reply_to= [data['email']]
-        )
-        email_message.content_subtype = 'html'
-
         try:
-            email_message.send()
-            print(f"✅ Email successfully sent to admin.")
+            resend.api_key = settings.RESEND_API_KEY
+
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",  # ← Keep this until you verify your domain
+                "to": ["aliabid1571@gmail.com"],  #Sales@apextourtravel.com
+                "reply_to": data['email'],
+                "subject": f"New Quote Request from {data['email']}",
+                "html": html_content,
+            })
+
+            print(f"✅ Email successfully sent!")
+
         except Exception as e:
-            print(f"error sending email: {e}")
+            print(f"❌ Error sending email: {e}")
             return HttpResponse("Error sending quote. Please try again.")
 
-        # 3. Return a success message (Partial HTML)
-
-
-    # If it's a GET request, just show the home page
     return render(request, 'home.html')
 
 from .models import VehicleCategory
@@ -75,6 +70,7 @@ def fleet(request):
 def about(request):
     if request== "POST":
         return render(request, 'fleet.html')
+
 
 
 
